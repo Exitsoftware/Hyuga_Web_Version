0387a30cba0ef5e2e78c08286633dd6ffd3fddb8
Na Yun Hwan <kbk9288@gmail.com>
2016-01-27 20:39:51 +0900

    Merge branch 'master' into pr/17
    # Conflicts:
    #	es_item_view.html

---
+++ test/factory.js
@@ -0,0 +1,17 @@
+var app = angular.module("test", ['ngCookies']);
+//app.factory('myFactory', function(){
+//    var savedData = {};
+//
+//    function set(data){
+//        savedData = data;
+//    }
+//
+//    function get(){
+//        return savedData;
+//    }
+//
+//    return{
+//        set: set, 
+//        get: get
+//    }
+//});
